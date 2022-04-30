1f01c6a537df85c83c631eeed3023e0701bda7ea
kaimuell <info@kaimueller-art.de>
2022-04-30 12:13:23 +0200

    added clear Action, cleaned up move action.

--- src/main/java/g6Agent/services/Direction.java
+++ src/main/java/g6Agent/services/Direction.java
@@ -1,5 +1,18 @@
 package g6Agent.services;
 
+import eis.iilang.Identifier;
+
 public enum Direction {
-    WEST, EAST, NORTH, SOUTH
+    WEST(new Identifier("w")), EAST(new Identifier("e")),
+    NORTH(new Identifier("n")), SOUTH(new Identifier("s"));
+
+    private final Identifier id;
+
+    Direction(Identifier id) {
+        this.id = id;
+    }
+
+    public Identifier getIdentifier() {
+        return id;
+    }
 }
