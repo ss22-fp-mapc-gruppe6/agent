ee626281adbeae64888960511ed1b77d34248b09
alimakW <kamila.wenzel@t-online.de>
2022-05-14 15:21:00 +0200

    Actions; move, attach, detach

---
+++ javaagents/src/main/java/g6Agent/actions/Connect.java
@@ -0,0 +1,33 @@
+package g6Agent.actions;
+
+import g6Agent.agents.MyTestAgent;
+import g6Agent.perceptionAndMemory.Enties.Block;
+import g6Agent.perceptionAndMemory.PerceptionAndMemory;
+import g6Agent.perceptionAndMemory.PerceptionAndMemoryImplementation;
+import g6Agent.services.Direction;
+import g6Agent.services.Point;
+
+// Two agents can connect blocks attached to them.
+public class Connect implements g6Action{
+
+    private final Direction direction;
+    private PerceptionAndMemory perception;
+    /*private final Block blockA;
+    private final MyTestAgent agentA;
+    private final Block blockB;
+    private final MyTestAgent agentB;**/
+
+    public Connect(Direction direction, String name) {
+        super();
+        this.direction = direction;
+        perception = new PerceptionAndMemoryImplementation();
+    }
+    @Override
+    public void setSucceededEffect(MyTestAgent agent, int step) {
+        if (perception.getLastAction().getSuccessMessage().equals("success")){
+            Point agentPosition = agent.getPosition(step);
+            //connect(agent, direction);
+            //connect(agent)
+        }
+    }
+}
