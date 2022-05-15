a0d5fba9d2a2bfb4883af1766b3c209ac03979f7
kaimuell <info@kaimueller-art.de>
2022-05-15 14:47:08 +0200

    interface for Goals

---
+++ javaagents/src/main/java/g6Agent/goals/Goal.java
@@ -0,0 +1,36 @@
+package g6Agent.goals;
+
+import g6Agent.actions.g6Action;
+
+/**
+ * Interface for Goals, which represent the desires and produce the intentions of an agent.
+ */
+public interface Goal {
+    /**
+     *
+     * @return the next Action to be taken to achieve this Goal
+     */
+    g6Action getNextAction();
+
+    /**
+     * Is this Goal still succeding with the current beliefs
+     * @return is it succeding?
+     */
+    boolean isSucceding();
+
+    /**
+     * The Goal is reached succesfully
+     * @return is the goal reached?
+     */
+    boolean isFullfilled();
+
+    //probably nonsense, should be decided outside and not hard coded
+    //int getPriority()
+
+    /**
+     * The Name of the Goal as String for Communication and other purposes
+     * @return the Name as String
+     */
+    String getName();
+
+}
