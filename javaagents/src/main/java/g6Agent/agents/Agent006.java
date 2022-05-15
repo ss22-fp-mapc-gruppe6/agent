cd219ea228ea1d248930dec991f7603d287f46fd
kaimuell <info@kaimueller-art.de>
2022-05-15 14:53:52 +0200

    Agent006

---
+++ javaagents/src/main/java/g6Agent/agents/Agent006.java
@@ -0,0 +1,54 @@
+package g6Agent.agents;
+
+import eis.iilang.Action;
+import eis.iilang.Percept;
+import g6Agent.MailService;
+import g6Agent.actions.g6Action;
+import g6Agent.decissionModule.DecisionModule;
+import g6Agent.decissionModule.TheStupidestDecisionModule;
+import g6Agent.goals.Goal;
+import g6Agent.perceptionAndMemory.Interfaces.PerceptionAndMemory;
+import g6Agent.perceptionAndMemory.PerceptionAndMemoryImplementation;
+
+
+/**
+ * The 2. best Agent
+ */
+public class Agent006 extends Agent{
+    private final PerceptionAndMemory perceptionAndMemory;
+    private final DecisionModule decisionModule;
+
+
+    /**
+     * Constructor
+     *
+     * @param name    the agent's name
+     * @param mailbox the mail facility
+     */
+    Agent006(String name, MailService mailbox) {
+        super(name, mailbox);
+        this.perceptionAndMemory = new PerceptionAndMemoryImplementation();
+        this.decisionModule = new TheStupidestDecisionModule(perceptionAndMemory);
+    }
+
+    @Override
+    public void handlePercept(Percept percept) {
+
+    }
+
+    @Override
+    public Action step() {
+        g6Action action = null;
+        perceptionAndMemory.handlePercepts(getPercepts());
+        if (perceptionAndMemory.isReadyForAction()){
+            Goal currentGoal = decisionModule.revalidateGoal();
+            action = currentGoal.getNextAction();
+        }
+        return (eis.iilang.Action) action;
+    }
+
+    @Override
+    public void handleMessage(Percept message, String sender) {
+
+    }
+}
