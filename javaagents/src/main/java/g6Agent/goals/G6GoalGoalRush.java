2099d3022981b09f0b5049798655163d0ad62367
kaimuell <info@kaimueller-art.de>
2022-05-19 12:28:33 +0200

    logic for is succeding

--- javaagents/src/main/java/g6Agent/goals/G6GoalGoalRush.java
+++ javaagents/src/main/java/g6Agent/goals/G6GoalGoalRush.java
@@ -1,14 +1,17 @@
 package g6Agent.goals;
 
 import g6Agent.actions.G6Action;
+import g6Agent.perceptionAndMemory.Enties.Block;
+import g6Agent.perceptionAndMemory.Enties.Task;
 import g6Agent.perceptionAndMemory.Interfaces.PerceptionAndMemory;
 
 public class G6GoalGoalRush implements Goal {
-
+    Task task;
     private final PerceptionAndMemory perceptionAndMemory;
 
     public G6GoalGoalRush(PerceptionAndMemory perceptionAndMemory) {
-    this.perceptionAndMemory = perceptionAndMemory;
+        this.perceptionAndMemory = perceptionAndMemory;
+        //check if attached blocks are part of a task, choose the task with an attached block matching
     }
 
 
@@ -16,6 +19,9 @@ public class G6GoalGoalRush implements Goal {
     public G6Action getNextAction() {
 
         //SubGoal : find Goal Zone
+
+        //if all blocks attached match task, rotate till matches pattern, -> submit
+
         //Subgoal : build Task together with other agents
 
 
@@ -24,6 +30,33 @@ public class G6GoalGoalRush implements Goal {
 
     @Override
     public boolean isSucceding() {
+        //Has no Blocks Attached
+        if(perceptionAndMemory.getAttachedBlocks().isEmpty()){
+            return false;
+        }
+        //Has no chosen Task
+        if(task == null){
+            return false;
+        }
+        //Chosen Task is still active
+        boolean taskStillActive = false;
+        for(Task t : perceptionAndMemory.getTasks()){
+            if (t.getName().equals(task.getName())){
+                taskStillActive=true;
+            }
+        }
+        if(!taskStillActive){
+            this.task = null;
+            return false;
+        }
+        //Has Blocks matching Task
+        for(Block b : task.getRequirements()){
+            for (Block blockAttached : perceptionAndMemory.getAttachedBlocks()){
+                if (blockAttached.getBlocktype().equals(b.getBlocktype())){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
