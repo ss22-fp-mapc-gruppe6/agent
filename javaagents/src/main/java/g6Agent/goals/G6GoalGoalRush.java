3610d4b5c53f6c9a44bd5a8a097a0e21bb637e76
kaimuell <info@kaimueller-art.de>
2022-05-20 11:39:12 +0200

    logic for isFullfilled()

--- javaagents/src/main/java/g6Agent/goals/G6GoalGoalRush.java
+++ javaagents/src/main/java/g6Agent/goals/G6GoalGoalRush.java
@@ -2,21 +2,37 @@ package g6Agent.goals;
 
 import g6Agent.actions.G6Action;
 import g6Agent.perceptionAndMemory.Enties.Block;
+import g6Agent.perceptionAndMemory.Enties.LastActionMemory;
+import g6Agent.perceptionAndMemory.Enties.Role;
 import g6Agent.perceptionAndMemory.Enties.Task;
 import g6Agent.perceptionAndMemory.Interfaces.PerceptionAndMemory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class G6GoalGoalRush implements Goal {
-    Task task;
+    private Task task;
     private final PerceptionAndMemory perceptionAndMemory;
 
     public G6GoalGoalRush(PerceptionAndMemory perceptionAndMemory) {
         this.perceptionAndMemory = perceptionAndMemory;
         //check if attached blocks are part of a task, choose the task with an attached block matching
+        for (Task task : perceptionAndMemory.getTasks()) {
+            for (Block b : task.getRequirements()) {
+                for (Block blockAttached : perceptionAndMemory.getAttachedBlocks()) {
+                    if (blockAttached.getBlocktype().equals(b.getBlocktype())) {
+                        this.task = task;
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public G6Action getNextAction() {
+
+
 
         //SubGoal : find Goal Zone
 
@@ -62,11 +78,28 @@ public class G6GoalGoalRush implements Goal {
 
     @Override
     public boolean isFullfilled() {
+        LastActionMemory lastAction = perceptionAndMemory.getLastAction();
+        if (lastAction == null) return false;
+        if (lastAction.getName().equals("submit") && lastAction.getSuccessMessage().equals("success")) return true;
         return false;
     }
 
     @Override
     public String getName() {
         return "G6GoalGoalRush";
+    }
+
+
+    private List<String> possibleRoleNames(){
+        List<String> roleNames = new ArrayList<>();
+        for(Role role : perceptionAndMemory.getPossibleRoles()){
+            boolean submitFound = false;
+            for (String action : role.getPossibleActions()){
+                if (action.equals("submit")){
+                    roleNames.add(role.getName());
+                }
+            }
+        }
+        return roleNames;
     }
 }
