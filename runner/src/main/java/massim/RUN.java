4ce70d3f0e4371126bad6f3515fb1a04e1f72f8c
mpnsk <email>
2022-06-21 15:26:29 +0200

    separate strings so intellij can jump into resources

--- runner/src/main/java/massim/RUN.java
+++ runner/src/main/java/massim/RUN.java
@@ -2,13 +2,15 @@ package massim;
 
 public class RUN {
     public static void main(String[] args) {
-        final String conf = "runner/src/main/resources/conf";
+        final String base = "runner/src/main/resources";
         final Thread agents = new Thread(() -> {
-            g6Agent.Main.main(new String[]{conf + "/agent/MyTestAgent"});
+            final String config = "/conf/agent/MyTestAgent";
+            g6Agent.Main.main(new String[]{base + config});
         });
         agents.setName("agents");
         final Thread server = new Thread(() -> {
-            massim.Server.main(new String[]{"-conf", conf + "/server/SampleConfig.json", "--monitor"});
+            final String config = "/conf/server/SampleConfig.json";
+            massim.Server.main(new String[]{"-conf", base + config, "--monitor"});
         });
         server.setName("server");
         server.start();
