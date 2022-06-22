97a1459d15f7973fbad688ead402a91a34fd232e
mpnsk <email>
2022-06-22 22:22:58 +0200

    move runner back into javaagents module

--- javaagents/src/main/java/g6Agent/RunBoth.java
+++ javaagents/src/main/java/g6Agent/RunBoth.java
@@ -1,15 +1,15 @@
-package massim;
+package g6Agent;
 
-public class RUN {
+public class RunBoth {
     public static void main(String[] args) {
-        final String base = "runner/src/main/resources";
+        final String base = "javaagents/src/main/resources";
         final Thread agents = new Thread(() -> {
-            final String config = "/conf/agent/MyTestAgent";
-            g6Agent.Main.main(new String[]{base + config});
+            final String config = "/conf/agent/MySingleAgent";
+            Main.main(new String[]{base + config});
         });
         agents.setName("agents");
         final Thread server = new Thread(() -> {
-            final String config = "/conf/server/SampleConfig.json";
+            final String config = "/conf/server/SmallConfig.json";
             massim.Server.main(new String[]{"-conf", base + config, "--monitor"});
         });
         server.setName("server");
