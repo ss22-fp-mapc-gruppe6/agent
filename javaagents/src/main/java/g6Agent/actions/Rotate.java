3c094305e3400bf17426e5e74a16c955c5de7b6e
mpnsk <email>
2022-07-04 14:48:06 +0200

    add predict rotate + test

--- javaagents/src/main/java/g6Agent/actions/Rotate.java
+++ javaagents/src/main/java/g6Agent/actions/Rotate.java
@@ -1,9 +1,19 @@
 package g6Agent.actions;
 
 import eis.iilang.Action;
+import g6Agent.perceptionAndMemory.Interfaces.PerceptionAndMemory;
+import g6Agent.services.Direction;
+import g6Agent.services.Point;
 import g6Agent.services.Rotation;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Rotate extends Action implements G6Action {
+
+    public final Rotation rotation;
     /**
      * Rotates the agent (and all attached things) 90 degrees in the given direction. For each attached thing, its final position after the rotation has to be free.
      *
@@ -11,5 +21,25 @@ public class Rotate extends Action implements G6Action {
      */
     public Rotate(Rotation rotation) {
         super("rotate", rotation.getIdentifier());
+        this.rotation = rotation;
     }
+
+
+    public boolean predictSuccess(final List<Point> attachments, final List<Point> obstacles) throws AttachmentCollidingWithObstacleException {
+        for (Point point : attachments) {
+            Point rotate = point.rotate(rotation);
+            if (obstacles.contains(rotate)) {
+                throw new AttachmentCollidingWithObstacleException(point, rotation, rotate);
+            }
+        }
+        return true;
+    }
+
+    @AllArgsConstructor
+    public static class AttachmentCollidingWithObstacleException extends Exception {
+        Point attachment;
+        Rotation rotation;
+        Point collision;
+    }
+
 }
