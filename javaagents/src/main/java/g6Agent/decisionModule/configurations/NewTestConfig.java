57a8f43891392b48b86be6babc9e0b61a7c95753
kaimuell <info@kaimueller-art.de>
2022-07-17 15:05:22 +0200

    implemented G6GoalDefendGoalZone

--- javaagents/src/main/java/g6Agent/decisionModule/configurations/NewTestConfig.java
+++ javaagents/src/main/java/g6Agent/decisionModule/configurations/NewTestConfig.java
@@ -15,7 +15,7 @@ public class NewTestConfig implements DecisionModuleConfiguration{
     public List<Goal> generateGoals(PerceptionAndMemory perceptionAndMemory, Strategy strategy) {
 
         List<Goal> goals = new ArrayList<>();
-        goals.add(new G6GoalExplore(perceptionAndMemory));
+        goals.add(new G6GoalExploreV2(perceptionAndMemory));
         goals.add(new G6GoalChangeRole(strategy.getPreferredRoleName(), perceptionAndMemory));
         goals.add(new G6GoalRetrieveBlock(perceptionAndMemory));
         for (Task task : perceptionAndMemory.getActiveTasks()){
@@ -27,16 +27,25 @@ public class NewTestConfig implements DecisionModuleConfiguration{
 
     @Override
     public double priority(Goal goal, Strategy strategy) {
-        if (strategy.equals(Strategy.OFFENSE)) {
-            if (goal instanceof G6GoalExplore) return 0.0;
-            if (goal instanceof G6GoalChangeRole) return 1.0;
-            if (goal instanceof G6GoalFulfillSingleTaskV1) return 2.0;
+        switch (strategy){
+            case OFFENSE -> {
+                if (goal instanceof G6GoalExploreV2) return 0.0;
+                if (goal instanceof G6GoalChangeRole) return 1.0;
+                if (goal instanceof G6GoalFulfillSingleTaskV1) return 2.0;
+                return -1;
+            }
+            case DEFENSE -> {
+                if (goal instanceof G6GoalExploreV2) return 0.0;
+                if (goal instanceof G6GoalDefendGoalZone) return 1.0;
+                if (goal instanceof G6GoalChangeRole) return 2.0;
+                return -1;
+            }
         }
         return -1;
     }
 
     @Override
     public double getMaxOffensivePercentage() {
-        return 1.0;
+        return 0.8;
     }
 }
--- javaagents/src/main/java/g6Agent/goals/G6GoalDefendGoalZone.java
+++ javaagents/src/main/java/g6Agent/goals/G6GoalDefendGoalZone.java
@@ -1,7 +1,18 @@
 package g6Agent.goals;
 
+import g6Agent.actions.Clear;
 import g6Agent.actions.G6Action;
+import g6Agent.actions.Skip;
+import g6Agent.decisionModule.astar.AStar;
+import g6Agent.decisionModule.manhattanDistanceMove.ManhattanDistanceMove;
 import g6Agent.perceptionAndMemory.Interfaces.PerceptionAndMemory;
+import g6Agent.services.Direction;
+import g6Agent.services.Point;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class G6GoalDefendGoalZone implements Goal {
     private final PerceptionAndMemory perceptionAndMemory;
@@ -13,7 +24,76 @@ public class G6GoalDefendGoalZone implements Goal {
 
     @Override
     public G6Action getNextAction() {
-        return null;
+        //If sees EnemyAgent next to a Block clear action against Enemy Agent
+        Point target =determineIfSeesEnemyAgentWithBlockAttached();
+        if(target != null){
+            return moveToTargetAndTackle(target);
+        }
+
+        //check if in GoalZone, if yes -> skip, else move to GoalZone
+        return moveToClosestGoalZone();
+    }
+
+    @NotNull
+    private G6Action moveToClosestGoalZone() {
+        boolean isInGoalZone = perceptionAndMemory.getGoalZones().stream().anyMatch(point-> point.equals(new Point(0,0)));
+        boolean doesntKnowAGoalZone = perceptionAndMemory.getGoalZones().isEmpty();
+        if (isInGoalZone || doesntKnowAGoalZone){
+            return new Skip();
+        }
+        Point closestGoalZone = perceptionAndMemory.getGoalZones()
+                .stream()
+                .min(Comparator.comparingInt(goalZone -> goalZone.manhattanDistanceTo(new Point(0,0))))
+                .orElse(null);
+        if (closestGoalZone == null) return new Skip();
+        return moveTo(closestGoalZone);
+    }
+
+    @NotNull
+    private G6Action moveToTargetAndTackle(Point target) {
+        if (target.manhattanDistanceTo(new Point(0,0)) <= perceptionAndMemory.getCurrentRole().getClearActionMaximumDistance()){
+            G6Action tackleAgent  = new Clear(target);
+            if (tackleAgent.predictSuccess(perceptionAndMemory)) return tackleAgent;
+        }
+        Point closestPointNextToTarget = closestUnblockedPointNextTo(target);
+
+        return moveTo(closestPointNextToTarget);
+    }
+
+    private Point closestUnblockedPointNextTo(Point target) {
+       return Arrays
+                .stream(Direction.allDirections())
+                .map(direction -> direction.getNextCoordinate().add(target))
+                .filter(point-> perceptionAndMemory.getBlocks()
+                        .stream()
+                        .noneMatch(block -> block.getCoordinates().equals(point)))
+                .min(Comparator.comparingInt(point -> point.manhattanDistanceTo(new Point(0,0))))
+                .orElse(target);
+    }
+
+    @NotNull
+    private G6Action moveTo(Point closestPointNextToTarget) {
+        G6Action moveToTarget = AStar.astarNextStep(closestPointNextToTarget, perceptionAndMemory)
+                .orElse(ManhattanDistanceMove.nextAction(closestPointNextToTarget, perceptionAndMemory));
+        if (moveToTarget.predictSuccess(perceptionAndMemory)) return moveToTarget;
+        moveToTarget = AStar.astarNextStepWithAgents(closestPointNextToTarget, perceptionAndMemory)
+                .orElse(ManhattanDistanceMove.nextAction(closestPointNextToTarget, perceptionAndMemory));
+        return moveToTarget;
+    }
+
+    private Point determineIfSeesEnemyAgentWithBlockAttached() {
+        if (perceptionAndMemory.getCurrentRole() == null) return null;
+        List<Point> enemyAgentsNextToABlock = perceptionAndMemory.getEnemyAgents()
+                .stream()
+                .filter(agent -> perceptionAndMemory.getBlocks()    //has blocks next to ist
+                        .stream()
+                        .anyMatch(block -> block.getCoordinates().isAdjacentTo(agent)))
+                .filter(agent-> agent.manhattanDistanceTo(new Point(0,0)) <= perceptionAndMemory.getCurrentRole().getVisionRange()) //is in vision range
+                .toList();
+
+        if (enemyAgentsNextToABlock.isEmpty()) return null;
+
+        return enemyAgentsNextToABlock.stream().min(Comparator.comparingInt(x-> x.manhattanDistanceTo(new Point(0,0)))).orElse(null);
     }
 
     @Override
